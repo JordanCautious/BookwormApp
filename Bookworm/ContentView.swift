5f8141a361659b5948fd0947c06a5235a491352c
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-18 05:04:48 -0400

    In the process of building Bookworm

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -8,14 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(sortDescriptors: []) var students: FetchedResults<Student>
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            List(students) { student in
+                Text(student.name ?? "Unknown")
+            }
+            
+            Button("Add") {
+                let firstNames = ["Ginny", "Harry", "Hermione", "Luna", "Ron", "Jordan"]
+                let lastNames = ["Granger", "Lovegood", "Potter", "Weasley", "Haynes"]
+                
+                let chosenFirstName = firstNames.randomElement()!
+                let chosenLastName = lastNames.randomElement()!
+                
+                let student = Student(context: moc)
+                student.id = UUID()
+                student.name = "\(chosenFirstName) \(chosenLastName)"
+                
+                try? moc.save()
+            }
         }
-        .padding()
     }
 }
 
---
+++ Bookworm/DataController.swift
@@ -0,0 +1,20 @@
+//
+//  DataController.swift
+//  Bookworm
+//
+//  Created by Jordan Haynes on 9/18/23.
+//
+import CoreData
+import Foundation
+
+class DataController: ObservableObject {
+    let container = NSPersistentContainer(name: "Bookworm")
+    
+    init() {
+        container.loadPersistentStores { description, error in
+            if let error = error {
+                print("Core Data failed to load: \(error.localizedDescription)")
+            }
+        }
+    }
+}
