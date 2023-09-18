717d45025ef9d0bca119f1605e074cd18a0b5fbe
Jordan Haynes <88909614+JordanCautious@users.noreply.github.com>
2023-09-18 17:30:27 -0400

    Added a way for users to delete books

--- Bookworm/ContentView.swift
+++ Bookworm/ContentView.swift
@@ -9,7 +9,10 @@ import SwiftUI
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) var moc
-    @FetchRequest(sortDescriptors: []) var books: FetchedResults<Book>
+    @FetchRequest(sortDescriptors: [
+        SortDescriptor(\.title),
+        SortDescriptor(\.author)
+    ]) var books: FetchedResults<Book>
     
     @State private var showingAddScreen = false
     
@@ -34,9 +37,13 @@ struct ContentView: View {
                         }
                     }
                 }
+                .onDelete(perform: deleteBooks)
             }
             .navigationTitle("Bookworm")
             .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    EditButton()
+                }
                 ToolbarItem(placement: .navigationBarTrailing) {
                     Button {
                         showingAddScreen.toggle()
@@ -49,6 +56,15 @@ struct ContentView: View {
         .sheet(isPresented: $showingAddScreen) {
             AddBookView()
         }
+    }
+    
+    func deleteBooks(at offsets: IndexSet) {
+        for offset in offsets {
+            let book = books[offset]
+            moc.delete(book)
+        }
+        
+//        try? moc.save()
     }
 }
 
